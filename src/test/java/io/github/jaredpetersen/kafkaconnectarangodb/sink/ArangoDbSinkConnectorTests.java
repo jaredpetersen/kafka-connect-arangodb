7bcbea59eb18931cec5cd5bcf8645bcf205e9f46
Jared Petersen <jaredtoddpetersen@gmail.com>
2019-01-29 17:09:04 -0800

    made the development example work again

--- src/test/java/io/github/jaredpetersen/kafkaconnectarangodb/sink/ArangoDbSinkConnectorTests.java
+++ src/test/java/io/github/jaredpetersen/kafkaconnectarangodb/sink/ArangoDbSinkConnectorTests.java
@@ -15,7 +15,7 @@ public class ArangoDbSinkConnectorTests {
   @Test
   public void versionReturnsVersion() {
     final SinkConnector connector = new ArangoDbSinkConnector();
-    assertEquals("1.0.2", connector.version());
+    assertEquals("1.0.3", connector.version());
   }
 
   @Test
